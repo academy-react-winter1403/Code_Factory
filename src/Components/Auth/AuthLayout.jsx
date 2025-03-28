ce852e64a1b6410118041e624267c957b1af3a04
AYaldaShabani <yalda.shabani27@gmail.com>
2025-03-29 01:17:32 +0330

    carete auth layout

---
+++ src/Components/Auth/AuthLayout.jsx
@@ -0,0 +1,10 @@
+import { div } from "framer-motion/client";
+
+const AuthLayout = () => { 
+    return (
+        <div>
+            Auth Layout
+        </div>
+    )
+}
+export default AuthLayout;
