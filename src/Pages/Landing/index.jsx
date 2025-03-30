0ff51d03b53b8cc50bb4f216270b89ce88a7fabf
sourin <101411011+sourinkaliji@users.noreply.github.com>
2025-03-30 17:45:04 +0330

    update landing

--- src/Pages/Landing/index.jsx
+++ src/Pages/Landing/index.jsx
@@ -1,8 +1,6 @@
+import { useLocation } from "react-router-dom";
+import Landing from "../../Components/Landing";
 const LandingPage = () => {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  );
+  return <Landing />;
 };
 export default LandingPage;
