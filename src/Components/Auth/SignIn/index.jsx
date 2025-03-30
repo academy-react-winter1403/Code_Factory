d4cef77ae7ccc6cbe34a0860ed1a763220c31b98
sourin <101411011+sourinkaliji@users.noreply.github.com>
2025-03-30 17:43:59 +0330

    bug fixed

--- src/Components/Auth/SignIn/index.jsx
+++ src/Components/Auth/SignIn/index.jsx
@@ -1,11 +1,5 @@
-import { h1 } from "framer-motion/client"
-import AuthLayout from "../AuthLayout"
+const SignIn = () => {
+  return <h1>this sign in is from componnet</h1>;
+};
 
-const SignIn = () => { 
-    return (
-        <AuthLayout />
-        // <h1>this sign in is from componnet</h1>
-    )
-}
-
-export default SignIn
+export default SignIn;
