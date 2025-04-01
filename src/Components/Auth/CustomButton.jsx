6a76917d655a2cc68dd8a39a00b014ac2abd0182
AYaldaShabani <yalda.shabani27@gmail.com>
2025-04-01 23:56:52 +0330

    add custom Button

---
+++ src/Components/Auth/CustomButton.jsx
@@ -0,0 +1,8 @@
+const CustomButton = ({text}) => {
+    return (
+        <button type="submit" className="w-full bg-[#FFC224] border-[1px] py-3 px-7  rounded-full font-semibold
+        shadow-[3px_4px] hover:cursor-pointer
+        ">{text}</button>
+ )   
+}
+export default CustomButton
