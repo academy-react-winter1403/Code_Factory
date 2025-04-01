acff927f4e835a7efadc9da9eb74237d56673484
AYaldaShabani <yalda.shabani27@gmail.com>
2025-04-02 02:23:57 +0330

    add sign up step three page

---
+++ src/Pages/Auth/SignUp/SignUpThreePage.jsx
@@ -0,0 +1,8 @@
+import SignUpThree from "../../../Components/Auth/SignUp/SignUpThree"
+
+const SignUpThreePage = () => {
+    return (
+        <SignUpThree />
+    )
+}
+export default SignUpThreePage
