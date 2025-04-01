430ffb98b298edf7e863be6c8f3fb0a2f4b7663c
sourin <101411011+sourinkaliji@users.noreply.github.com>
2025-04-01 15:24:04 +0330

    add navbarMd to header

--- src/Components/Common/Header/NavbarMd/index.jsx
+++ src/Components/Common/Header/NavbarMd/index.jsx
@@ -1,9 +1,24 @@
 const NavbarMd = () => {
-    return (
-        <div>
-            this is a Md navbar
-        </div>
-    )
+  return (
+    <div className="flex justify-around h-[125]">
+      <div className="flex justify-between">
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
 
-}
-export default NavbarMd
+      <div className="flex justify-between">
+        <div className="flex"></div>
+        <div className="flex justify-between">
+          <div className=" text-[10px]">صفحه اصلی</div>
+          <div className="">دوره های آموزشی</div>
+          <div className="">ایونت ها</div>
+          <div className="">فروشگاه</div>
+          <div className="">وبلاگ</div>
+          <div className="">مربیان</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NavbarMd;
